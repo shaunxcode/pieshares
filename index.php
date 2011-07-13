bef90ac16560480952df4ce97ba362efd20ba6c9
shaun gilchrist <shaunxcode@gmail.com>
2011-07-12 23:20:28 -0600

    7th iteration - editing name now works for all nodes, also selects text after focus and blur cancels

--- index.php
+++ index.php
@@ -122,12 +122,12 @@
 			text-align: center;
 		}
 
-		.TaskTitle { 
+		.TaskTitle, .ActionTitle, .WorkTitle { 
 			float: left;
 			width: 75%; 
 		}
 
-		.TaskTitle input {
+		input.NameInput {
 			font-size: inherit;
 			width: 100%;
 			font-family: inherit;
@@ -142,10 +142,43 @@
 		}
 	</style>
 	<script>
+		var PS;
 		$(function(){
-			var PS = {
+			PS = {
 				localTaskId: 0,
 				tasks: {},
+				actions: {},
+				works: {},
+
+				_nameEditor: function(self) {
+					return function() {
+						if(!self.editing) {
+							self.editing = true;
+							$(this)
+								.html(
+									$('<input />')
+										.addClass('NameInput')
+										.val(self.name)
+										.blur(function(){
+											$(this).replaceWith(self.name);
+											self.editing = false;
+										})
+										.keypress(function(evt) {
+											if(evt.keyCode == 13) {
+												self.name = $(this).val();
+												$(this).replaceWith(self.name);
+												self.editing = false;
+											}
+											if(evt.keyCode == 27) {
+												$(this).replaceWith(self.name);
+												self.editing = false;
+											}
+										}));
+
+							$('input', this).focus().select();
+						}		
+					};
+				},
 
 				task: function(id, name, view) {
 					this.id = id;
@@ -159,35 +192,57 @@
 							$('<div />')
 								.addClass('TaskTitle')
 								.text(name)
-								.click(function() {
-									if(!self.editing) {
-										self.editing = true;
-										$(this)
-											.html(
-												$('<input />')
-													.val(self.name)
-													.keypress(function(evt) {
-														if(evt.keyCode == 13) {
-															self.name = $(this).val();
-															$(this).replaceWith(self.name);
-															self.editing = false;
-														}
-														if(evt.keyCode == 27) {
-															$(this).replaceWith(self.name);
-															self.editing = false;
-														}
-													}));
-
-										$('input', this).focus();
-									}
-								}))
+								.click(PS._nameEditor(self)))
 				},
 
 				newTask: function(taskEl) {
 					var view = $('> div', taskEl);
 					var id = 'task-' + PS.localTaskId++;
 					return PS.tasks[id] = new PS.task(id, view.text(), view);
+				},
+
+				action: function(id, name, view) {
+					this.id = id;
+					this.name = name;
+					this.view = view;
+
+					var self = this;
+
+					this.view
+						.html(
+							$('<div />')
+								.addClass('ActionTitle')
+								.text(name)
+								.click(PS._nameEditor(self)))
+				},
+
+				newAction: function(task, actionEl) {
+					var view = $('> div', actionEl);
+					var id = 'action-' + PS.localTaskId++;
+					return PS.actions[id] = new PS.action(id, view.text(), view);
+				},
+
+				work: function(id, name, view) {
+					this.id = id;
+					this.name = name;
+					this.view = view;
+
+					var self = this;
+
+					this.view
+						.html(
+							$('<div />')
+								.addClass('WorkTitle')
+								.text(name)
+								.click(PS._nameEditor(self)))
+				},
+
+				newWork: function(action, workEl) {
+					var view = $('> div', workEl);
+					var id = 'work-' + PS.localTaskId++;
+					return PS.works[id] = new PS.work(id, view.text(), view);
 				}
+
 			};
 
 			$('#ProjectTasks')
@@ -201,7 +256,7 @@
 						$('.FirstItem', this).remove();
 						var task = $(ui.draggable);
 						if(!task.data('children')) {
-							PS.newTask(task);
+							var taskNode = PS.newTask(task);
 
 							task
 								.append(
@@ -216,6 +271,8 @@
 												$('.FirstActionItem', this).remove();
 												var action = $(ui.draggable);
 												if(!action.data('children')) {
+													var actionNode = PS.newAction(taskNode, action); 
+
 													action
 														.append(
 															$('<ul />')
@@ -227,6 +284,11 @@
 																	accept: '.Work',
 																	drop: function(event, ui) {
 																		$('.FirstWorkItem', this).remove();
+																		var work = $(ui.draggable);
+																		if(!work.data('init')) {
+																			var workNode = PS.newWork(actionNode, work);
+																			work.data('init', true);
+																		}
 																	}
 																})
 																.addClass('Works')
