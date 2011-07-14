a3803da3cfb28ae801b5bef8f15b2fef868a8b55
shaun gilchrist <shaunxcode@gmail.com>
2011-07-14 10:54:42 -0600

    iteration 32 - fixed bug where you couldnt add items once it started to overflow

--- index.php
+++ index.php
@@ -24,95 +24,99 @@
 	</script>
 </head>
 <body>
-	<div id="Header">
-		<div class="container">
-			<div class="span-4 first"><img src="images/logo-1.png" alt="PieShares"/></div>
-			<div class="span-16 ProjectName"><h1><?php echo $file; ?></h1></div>
-			<div class="span-4 last">
-				<div id="UserSettingsMenu">
-					<img src="images/icon-user.png" alt="profile" />
-					<img src="images/icon-settings.png" alt="settings" />
-					<img src="images/icon-exit.png" alt="logout" />
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="container" id="AppContainer">
-		<div class="span-4 first last" id="Sidebar">		
-			<h3 id="ProjectMenuHeading">Project Details</h3>
-				<div id="ProjectMenu">
-					<ul>
-						<li>Overview</li>
-						<li>Task Tree</li>
-						<li>Users(<span id="UserCount">n</span>)</li>
-						<li>Items<ul>
-							<li>Tasks(<span id="TaskCount">n</span>)</li>
-							<li>Actions(<span id="ActionCount">n</span>)</li>
-							<li>Work(<span id="WorkCount">n</span>)</li></ul><li>
-					</ul>
-				</div>
-			<h3 id="UsersMenuHeading">Active Users</h3>
-			<div id="UserMenu">
-				<ul><li>username</li><li>username</li></ul>
-			</div>
-			<h3 id="ContributionsMenuHeading">Contributions</h3>
-			<div id="ContributionMenu">
-				<ul><li>Type of contrib.</li><li>Type of Contrib.</li></ul>
-			</div>
-		</div>
-		<div id="WorkSpace" class="span-20 last">
-			<div id="InnerWorkSpace">
-				<div id="ProjectSection" class="span-20 first last Section">
-					<div class="span-5 first">
-				   	 	&nbsp;
-				   	 	<div id="TemplatesContainer">
-				   	 		<span>Drag n' Drop</span>
-							<ul id="ItemTemplates" class="Tasks clear">
-								<li class="Task">
-									<div class="ItemInner">
-										<div class="ItemBot"></div>
-										<div class="ItemTop">
-											<span>Task</span> 
-											<img src="images/handle.png" alt="handle"/>
-										</div>
-									</div>
-								</li>
-								<li class="Action">
-									<div class="ItemInner">
-										<div class="ItemBot"></div>
-										<div class="ItemTop">
-											<span>Action</span> 
-											<img src="images/handle.png" alt="handle" />
-										</div>
-									</div>
-								</li>
-								<li class="Work">
-									<div class="ItemInner">
-										<div class="ItemBot"></div>
-										<div class="ItemTop">
-											<span>Work</span> 
-											<img src="images/handle.png" alt="handle" />
-										</div>
-									</div>
-								</li>
-							</ul>
+	<div id="wrap">
+		<div id="main" class="container">
+			<div id="Header">
+				<div class="container">
+					<div class="span-4 first"><img src="images/logo-1.png" alt="PieShares"/></div>
+					<div class="span-16 ProjectName"><h1><?php echo $file; ?></h1></div>
+					<div class="span-4 last">
+						<div id="UserSettingsMenu">
+							<img src="images/icon-user.png" alt="profile" />
+							<img src="images/icon-settings.png" alt="settings" />
+							<img src="images/icon-exit.png" alt="logout" />
 						</div>
 					</div>
-					<div class="span-15 last">
-						<ul id="ProjectTasks" class="Tasks">
-							<li class="FirstItem">Drag Task Here</li>
-						</ul>
+				</div>
+			</div>
+
+			<div class="container" id="AppContainer">
+				<div class="span-4 first last" id="Sidebar">		
+					<h3 id="ProjectMenuHeading">Project Details</h3>
+						<div id="ProjectMenu">
+							<ul>
+								<li>Overview</li>
+								<li>Task Tree</li>
+								<li>Users(<span id="UserCount">n</span>)</li>
+								<li>Items<ul>
+									<li>Tasks(<span id="TaskCount">n</span>)</li>
+									<li>Actions(<span id="ActionCount">n</span>)</li>
+									<li>Work(<span id="WorkCount">n</span>)</li></ul><li>
+							</ul>
+						</div>
+					<h3 id="UsersMenuHeading">Active Users</h3>
+					<div id="UserMenu">
+						<ul><li>username</li><li>username</li></ul>
+					</div>
+					<h3 id="ContributionsMenuHeading">Contributions</h3>
+					<div id="ContributionMenu">
+						<ul><li>Type of contrib.</li><li>Type of Contrib.</li></ul>
 					</div>
 				</div>
-				<div id="UsersSection" class="span-20 first last Section">
-					<div class="SectionInner">
-					<img src="images/users.png" />
-					</div>
-				</div>
-				<div id="ContributionsSection" class="span-20 first last Section">
-					<div class="SectionInner">
-					<img src="images/contributions.png" />
+				<div id="WorkSpace" class="span-20 last">
+					<div id="InnerWorkSpace">
+						<div id="ProjectSection" class="span-20 first last Section">
+							<div class="span-5 first">
+						   	 	&nbsp;
+						   	 	<div id="TemplatesContainer">
+						   	 		<span>Drag n' Drop</span>
+									<ul id="ItemTemplates" class="Tasks clear">
+										<li class="Task">
+											<div class="ItemInner">
+												<div class="ItemBot"></div>
+												<div class="ItemTop">
+													<span>Task</span> 
+													<img src="images/handle.png" alt="handle"/>
+												</div>
+											</div>
+										</li>
+										<li class="Action">
+											<div class="ItemInner">
+												<div class="ItemBot"></div>
+												<div class="ItemTop">
+													<span>Action</span> 
+													<img src="images/handle.png" alt="handle" />
+												</div>
+											</div>
+										</li>
+										<li class="Work">
+											<div class="ItemInner">
+												<div class="ItemBot"></div>
+												<div class="ItemTop">
+													<span>Work</span> 
+													<img src="images/handle.png" alt="handle" />
+												</div>
+											</div>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="span-15 last">
+								<ul id="ProjectTasks" class="Tasks">
+									<li class="FirstItem">Drag Task Here</li>
+								</ul>
+							</div>
+						</div>
+						<div id="UsersSection" class="span-20 first last Section">
+							<div class="SectionInner">
+							<img src="images/users.png" />
+							</div>
+						</div>
+						<div id="ContributionsSection" class="span-20 first last Section">
+							<div class="SectionInner">
+							<img src="images/contributions.png" />
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
